a16fe79a837bb1689475c0b828dc46b47bb668b0
Mariusz Krezel <GreenFrogContact@gmail.com>
2021-01-26 22:46:43 +0000

    backup commit6

--- deploy.sh
+++ deploy.sh
@@ -437,11 +437,11 @@ case $mainmenu_selection in
 
 		sleep 7
 		echo -e "\e[36;1m    Restore completed\e[0m"
-        echo -e "\e[32m======================================================\e[0m"
+        echo -e "\e[32m=====================================================================================\e[0m"
 
 	else
-		echo -e "\e[32m======================================================\e[0m"
-		echo -e "\e[36;1m    \e[34;1mrclone\e[0m not installed or \e[34;1m(gdrive)\e[0m not configured \e[32;1mchecking local backup\e[0m"
+		echo -e "\e[32m=====================================================================================\e[0m"
+		echo -e "\e[36;1m    \e[34;1mrclone\e[0m \e[36;1m not installed or \e[34;1m(gdrive)\e[0m \e[36;1m not configured \e[32;1mchecking local backup\e[0m"
 
 			if [ -e '~/LMDS/LMDSBackups/LMDS*' ] ; then
     			echo -e "    \e[41m    NO LOCAL BACKUP FILES FOUND!!!    \e[0m"

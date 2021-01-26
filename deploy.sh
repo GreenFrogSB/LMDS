2f94afd91b00d4c861bd0fc83c89e9e24828dda6
Mariusz Krezel <GreenFrogContact@gmail.com>
2021-01-26 17:47:26 +0000

    ready for testing 2

--- deploy.sh
+++ deploy.sh
@@ -415,11 +415,11 @@ case $mainmenu_selection in
 	    
 		# no check if online - mayve some another time just assume it is online 
 		echo -e "\e[32m======================================================\e[0m"
-		echo -e "\e[36m;1m    Sync with Google Drive \e[32;1msucessfull\e[0m"
+		echo -e "\e[36;1m    Sync with Google Drive \e[32;1msucessfull\e[0m"
 
 		# check for recent backup file 
 	 	restorefile="$(ls -t1 ~/LMDS/LMDSBackups/LMDS* | head -1 | grep -o 'LMDSbackup.*')"
-		echo -e "\e[36m;1m    Restoring \e[32;1m $restorefile\e[0m"
+		echo -e "\e[36;1m    Restoring \e[32;1m $restorefile\e[0m"
  
 		# stop all container
 		echo -e "\e[36;1m    Stopping all containers\e[0m"
@@ -449,15 +449,15 @@ case $mainmenu_selection in
 		echo "\e[36;1m    Local backup found \e[32;1mrestoring from local backup\e[0m"
 
 		# stop all container
-		echo -e "   \e[36;1m Stopping all containers\e[0m"
+		echo -e "\e[36;1m    Stopping all containers\e[0m"
 		sudo docker stop $(docker ps -a -q) 
 
 		# owerwrite all container
-		echo -e "   \e[36;1m Restoring all containers from local backup\e[0m"
+		echo -e "\e[36;1m    Restoring all containers from local backup\e[0m"
 		sudo tar -xzf "$(ls -t1 ~/LMDS/LMDSBackups/LMDS* | head -1)" -C ~/LMDS/
 
 		# start all containers from docker-comose/yml
-		echo -e "   \e[36;1m Starting all containers\e[0m"
+		echo -e "\e[36;1m    Starting all containers\e[0m"
 		docker-compose up -d
 
 		sleep 7

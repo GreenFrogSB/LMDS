3a185e92baa8155d29f5b33f207719620a034924
Mariusz Krezel <GreenFrogContact@gmail.com>
2021-06-25 12:03:20 +0100

    Docker-compose pip updater correction

--- scripts/update_compose.sh
+++ scripts/update_compose.sh
@@ -8,7 +8,7 @@ echo  -e "\e[33;1m    Stopping Docker-compose\e[0m"
 docker-compose stop
 
 echo  -e "\e[33;1m    Removing Docker-compose\e[0m"
-sudo apt-get remove docker-compose &> /dev/null
+sudo apt-get remove docker-compose -y &> /dev/null
 
 echo  -e "\e[33;1m    Getting Python3\e[0m"
 sudo apt-get install libffi-dev libssl-dev -y &> /dev/null
@@ -16,7 +16,7 @@ sudo apt-get install python3-dev -y &> /dev/null
 sudo apt-get install python3 python3-pip -y &> /dev/null
 
 echo  -e "\e[33;1m    Installing new Docker-compose via pip3\e[0m"
-sudo pip3 install docker-compose &> /dev/null
+sudo pip3 install docker-compose -y &> /dev/null
 
 echo  -e "\e[33;1m    Starting stack up again\e[0m"
 docker-compose up -d
